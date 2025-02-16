ea2020ba61722680a01099f6a0b5d8e5fc91dc1a
JeonGAMJA <indian_potato@naver.com>
2025-02-16 18:57:00 +0900

    feat: Console 클래스 구현

---
+++ src/Console.js
@@ -0,0 +1,21 @@
+class Console {
+  static async readLine(message) {
+    const readline = require("readline");
+    const rl = readline.createInterface({
+      input: process.stdin,
+      output: process.stdout,
+    });
+
+    console.log(message);
+    return new Promise((resolve) => {
+      rl.question("", (answer) => {
+        rl.close();
+        resolve(answer);
+      });
+    });
+  }
+
+  static print(message) {
+    console.log(message);
+  }
+}
